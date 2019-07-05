1b4119a8f3676b77723441de16cbda3518bd777c
Ben Coughlan <ben@liquidinstruments.com>
2019-07-05 16:58:42 +1000

    HG-3317 Added new patch version

--- pymoku/version.py
+++ pymoku/version.py
@@ -4,10 +4,10 @@ import pkg_resources as pkr
 compat_fw = [501]
 
 # List of compatible patches
-compat_patch = [0]
+compat_patch = [1]
 
 # List of compatible packs
-compat_packs = []
+compat_packs = [('python-py', '7439434C8782832F134C21090D4E32DB49896171\n')]
 
 # Compatible network protocol version
 protocol_version = '7'
