e8ff7a2ebb4fb9f99001ba5bab41264b9781c0b3
Tim Lam <tim@liquidinstruments.com>
2019-07-18 15:40:05 +1000

    PM-513 : style changes.

--- pymoku/_bode_analyzer.py
+++ pymoku/_bode_analyzer.py
@@ -3,6 +3,7 @@ from ._instrument import deprecated
 
 
 class BodeAnalyzer(_frequency_response_analyzer.FrequencyResponseAnalyzer):
-    @deprecated(category='class', message='BodeAnalyzer is deprecated; use FrequencyResponseAnalyzer')
+    @deprecated(category='class', message='BodeAnalyzer is deprecated;'
+                                          ' use FrequencyResponseAnalyzer')
     def __init__(self):
         super(BodeAnalyzer, self).__init__()
