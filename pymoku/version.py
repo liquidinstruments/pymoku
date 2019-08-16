4c9dbb3459bbd9d2fef2f148d2c4e512c33417f1
Ben Coughlan <ben@liquidinstruments.com>
2019-08-16 14:50:48 +1000

    HG-3298 bumping version numbers for 1.9

--- pymoku/version.py
+++ pymoku/version.py
@@ -1,16 +1,16 @@
 import pkg_resources as pkr
 
 # List of compatible firmware builds
-compat_fw = [501]
+compat_fw = [510]
 
 # List of compatible patches
-compat_patch = [1]
+compat_patch = [0]
 
 # List of compatible packs
-compat_packs = [('python-py', '7439434C8782832F134C21090D4E32DB49896171\n')]
+compat_packs = []
 
 # Compatible network protocol version
-protocol_version = '7'
+protocol_version = '8'
 
 # Official release name
 release = pkr.get_distribution("pymoku").version
